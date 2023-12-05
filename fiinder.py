b0c739121a7b68f01e0ca62b3c82ce63f81baa7b
Alex Hawley <102092963+alxHawley@users.noreply.github.com>
2023-12-04 17:57:30 -0800

    signal quality indicator added
    * added signal quality indicator, images
    * utils and modem_comm created to handle modem interaction
    * images for GPS signal added

--- fiinder.py
+++ fiinder.py
@@ -1,26 +1,56 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGridLayout
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWebEngineWidgets import QWebEngineProfile
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
-from PyQt5.QtCore import QUrl
-from PyQt5.QtCore import QTimer
 import sys
 import os
 import subprocess
+import time
+from utils.modem_comm import open_modem_connection, send_at, signal_quality_indicator
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGridLayout, QSpacerItem, QSizePolicy
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWebEngineWidgets import QWebEngineProfile
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QThread, pyqtSignal
 from pytryfi import PyTryFi
 import requests
 import gpsd
 
 
+class SignalQualityThread(QThread):
+    signal_quality_updated = pyqtSignal(int)
+
+    def run(self):
+        port = "/dev/ttyUSB2"
+        ser = open_modem_connection(port)
+        if ser is not None:
+            while True:
+                self.update_signal_quality(ser)
+                time.sleep(5)  # Delay for 5 seconds
+
+    def update_signal_quality(self, ser):
+        response = send_at(ser)
+        print(response)
+        # parse response to get rssi and rsrq values
+        for line in response:
+            signal_info = line.decode('utf-8').strip()  # Remove newline characters
+            if signal_info.startswith('+CSQ:'):
+                rssi, rsrq = [int(x) for x in signal_info.split(':')[1].split(',')]  # Split the line to get rssi and rsrq
+                signal_quality = signal_quality_indicator(rssi, rsrq)
+                self.signal_quality_updated.emit(signal_quality)
+                print("Signal Quality: " + str(signal_quality))
+                break  # Exit the loop once we've found the +CSQ line
+        else:
+            print("No +CSQ line found in response")
+            self.signal_quality_updated.emit(-1)  # Emit special value for no CSQ
+
 class App(QWidget):
-    def __init__(self):
+    def __init__(self, skip_login=True):
         super().__init__()
 
         self.tryfi = None
         self.tracking = False
         self.timer = None
-        self.checkmark_label = None
 
         # create the layout
         self.layout = QGridLayout()
@@ -37,65 +67,81 @@ class App(QWidget):
         # create the map
         self.map = QWebEngineView()
         self.map.load(QUrl('http://localhost:5000'))
-        self.layout.addWidget(self.map, 3, 0, 1, 2)
+        self.layout.addWidget(self.map, 3, 0, 1, 9)
     
         # set the stretch factor for the map
         self.layout.setRowStretch(3, 1)
 
-        # set the stretch factor for the columns
-        self.layout.setColumnStretch(0, 1)
-        self.layout.setColumnStretch(1, 1)
-        
-        # create the login status label
-        self.login_status_label = QLabel(self)
-        self.login_status_label.setText("Logging in...")
-        self.login_status_label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.login_status_label, 0, 0)
+        # initialize gpsd and start updating GPS location
+        gpsd.connect()
 
-        # create the checkmark label
-        self.checkmark_label = QLabel(self)
-        self.checkmark_label.setPixmap(QPixmap('assets/checkmark-16.png'))
-        self.checkmark_label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.checkmark_label, 0, 1)
-        self.checkmark_label.hide()
+        # delay the first update_location call
+        QTimer.singleShot(5000, self.update_location)  # delay of 5 seconds
+
+        # font stuff
+        bold = QFont()
+        bold.setBold(True)
 
         # create tracking status label
         self.tracking_button = QPushButton('Start Tracking', self)
         self.tracking_button.clicked.connect(self.toggle_tracking)
+        self.tracking_button.setFixedSize(115, 30)
 
-        self.layout.addWidget(self.tracking_button, 1, 0, 1, 2)
+        self.layout.addWidget(self.tracking_button, 1, 0)
 
-        # create the latitude label
-        self.latitude_label = QLabel(self)
-        self.latitude_label.setText("Latitude: ")
-        self.layout.addWidget(self.latitude_label, 2, 0)
+        # create the signal quality indicator
+        self.signal_quality_icon = QLabel()
+        self.layout.addWidget(self.signal_quality_icon, 1, 8)
 
-        # create the longitude label
-        self.longitude_label = QLabel(self)
-        self.longitude_label.setText("Longitude: ")
-        self.layout.addWidget(self.longitude_label, 2, 1)
+        # Add a horizontal spacer at column 6 (or any column to the left of the indicator)
+        spacer = QSpacerItem(50, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.layout.addItem(spacer, 1, 7)
 
-        # initialize gpsd and start updating GPS location
-        gpsd.connect()
+        # signal quality threading
+        self.signal_quality_thread = SignalQualityThread()
+        self.signal_quality_thread.signal_quality_updated.connect(self.update_signal_quality_icon)
+        self.signal_quality_thread.start()
+        
+        if not skip_login:
+            self.login()
+ 
+    def update_signal_quality_icon(self, signal_quality):
+        print("Updating signal quality icon:", signal_quality)
+        # update the signal quality indicator icon based on signal_quality
+        if signal_quality == 1:
+            # set icon to excellent
+            pixmap = QPixmap('images/excellent.png')
+            print(pixmap.isNull())
+            self.signal_quality_icon.setToolTip('Excellent Signal')
+        elif signal_quality == 2:
+            # set icon to good
+            pixmap = QPixmap('images/good.png')
+            self.signal_quality_icon.setToolTip('Good Signal')
+        elif signal_quality == 3:
+            # set icon to ok
+            pixmap = QPixmap('images/ok.png')
+            self.signal_quality_icon.setToolTip('OK Signal')
+        elif signal_quality == 4:
+            # set icon to weak
+            pixmap = QPixmap('images/weak.png')
+            self.signal_quality_icon.setToolTip('Weak Signal')
+        elif signal_quality == 5:
+            # set icon to none
+            pixmap = QPixmap('images/none.png')
+            self.signal_quality_icon.setToolTip('No Signal')
+        else:
+            # set icon to error
+            pixmap = QPixmap('images/error.png')
+            self.signal_quality_icon.setToolTip('Error: No CSQ or unexpected value')
 
-        self.login()
-
-        # delay the first update_location call
-        QTimer.singleShot(5000, self.update_location)  # delay of 5000 milliseconds
+        pixmap = pixmap.scaled(30, 30, Qt.KeepAspectRatio)
+        self.signal_quality_icon.setPixmap(pixmap)
 
     def login(self):
         # initialize the PyTryFi object with the email and password
         email = os.getenv('TRYFI_EMAIL')
         password = os.getenv('TRYFI_PASSWORD')
         self.tryfi = PyTryFi(email, password)
-
-        # if the login was successful update ui
-        if self.tryfi is not None:
-            # update the login status label
-            self.login_status_label.setText("Login successful")
-
-            # show the checkmark label
-            self.checkmark_label.show()
 
     def update_location(self):
         # Get the current gpsd position
@@ -109,13 +155,15 @@ class App(QWidget):
         orientation = packet.track
 
         # Send the GPS data to the server
-        response = requests.post('http://localhost:5000/update_location', data={'latitude': latitude, 'longitude': longitude, 'orientation': orientation})
+        response = requests.post('http://localhost:5000/update_location', data={'latitude': latitude, 'longitude':
+                                                                            longitude, 'orientation': orientation},
+                                                                            timeout=5)
         if response.status_code == 200:
             print('Successfully sent GPS data to server')
             print('Received GPS data from server:', response.json())
         else:
             print('Failed to send GPS data to server')
-        
+    
         # Schedule the next update
         if self.timer is None or not self.timer.isActive():
             self.timer = QTimer()
@@ -167,8 +215,10 @@ class App(QWidget):
             latitude = pet._currLatitude
             longitude = pet._currLongitude
 
-            # Send the tracked object's location data to the server
-            response = requests.post('http://localhost:5000/update_tracked_location', data={'latitude': latitude, 'longitude': longitude})
+            # Send the collar location data to the server
+            response = requests.post('http://localhost:5000/update_tracked_location', data={'latitude': latitude,
+                                                                                            'longitude': longitude},
+                                                                                            timeout=5)
 
             if response.status_code == 200:
                 print('Successfully sent tracked object location data to server')
@@ -189,12 +239,14 @@ class App(QWidget):
                 self.timer.timeout.connect(self.fetch_location)
                 self.timer.start(15000)  # start the timer with an interval of 15000 milliseconds
 
-    def closeEvent(self, event):
+    def close_event(self, event):
+        """Override the default close event handler."""
         # stop the Flask server
         self.flask_process.terminate()
 
         # continue with the default close event handling
-        super().closeEvent(event)
+        super().close_event(event)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
---
+++ utils/modem_comm.py
@@ -0,0 +1,44 @@
+import serial
+import logging
+
+# open serial port
+def open_modem_connection(port, baud_rate=9600, timeout=1):
+    try:
+        ser = serial.Serial(port, baud_rate, timeout=timeout)
+        return ser
+    except serial.SerialException as e:
+        logging.error("Could not open serial port: %s", e)
+        return None
+
+# send AT for signal strength
+def send_at(ser):
+    ser.write(b'AT+CSQ\r\n')
+    response_lines = []
+    while True:
+        line = ser.readline()
+        response_lines.append(line)
+        if b'OK' in line or b'ERROR' in line:
+            break
+    return response_lines
+
+# interpret csq value
+def signal_quality_indicator(rssi, rsrq):
+    if rssi >= 31 and rsrq <= 3:
+        return 1 # "Excellent Signal"
+    elif rssi >= 22 and rsrq <= 3:
+        return 2 # "Very Good Signal"
+    elif rssi >= 12 and rsrq <= 6:
+        return 3 # "Good Signal"
+    elif rssi >= 2 and rsrq <= 7:
+        return 4 # "Signal may degrade"
+    else:
+        return 5 # "Signal quality not defined"
+
+def get_signal_quality(port, rssi, rsrq):
+    ser = open_modem_connection(port)
+    if ser is not None:
+        send_at(ser)
+        # parse response to get rssi and rsrq values
+        signal_quality = signal_quality_indicator(rssi, rsrq)
+        print("Signal Quality: " + str(signal_quality))
+        return signal_quality
